74fe9bc670d433d7dac26d98c69348a13a803b9a
Prince <prinsvaghasiya.it21@scet.ac.in>
2024-08-09 21:16:47 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,37 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About prince Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Welcome to PrinceEstate, your premier destination for exceptional real
+        estate services. With a deep understanding of the local market and a
+        passion for helping clients find their ideal homes, we are committed to
+        making your property journey seamless and successful. Whether you're a
+        first-time homebuyer or an experienced investor, our expert team is here
+        to provide you with personalized guidance and support every step of the
+        way.
+      </p>
+      <p className=" mb-4 text-slate-700">
+        At PrinceEstate, we believe that finding the perfect property is more
+        than just a transaction; it's a life-changing experience. Our diverse
+        portfolio features a wide range of properties, from cozy apartments to
+        luxurious estates, catering to various lifestyles and budgets. We pride
+        ourselves on our commitment to excellence, ensuring that each client
+        receives the highest level of service and attention to detail.
+      </p>
+      <p className="mb-4 text-slate-700 ">
+        As a trusted partner in the real estate industry, PrinceEstate is
+        dedicated to staying ahead of market trends and utilizing the latest
+        technology to enhance your buying or selling experience. Our team of
+        experienced professionals is always ready to go the extra mile to exceed
+        your expectations and help you achieve your real estate goals. Discover
+        the PrinceEstate difference and let us turn your property dreams into
+        reality.
+      </p>
+    </div>
+  );
 }
