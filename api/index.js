b506743238137620e0f7b120a210cd9ab3c64e98
Prince <prinsvaghasiya.it21@scet.ac.in>
2024-08-09 21:31:24 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,7 +5,9 @@ const userRouter = require("./routes/user.routes");
 const authRouter = require("./routes/auth.routes");
 const listingRouter = require("./routes/listing.router");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 dotenv.config();
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +25,12 @@ mongoose
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
